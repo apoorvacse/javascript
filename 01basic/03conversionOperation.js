3250ff4226116d9131b03b7dc4f2d6284a7b8d9b
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-06-06 19:36:02 +0000

    conversion of datatypes

---
+++ 01basic/03conversionOperation.js
@@ -0,0 +1,31 @@
+let score=false
+let scoree="22a"
+console.log(typeof(score))
+console.log(typeof scoree)
+//  it converts string type into nmber
+let valueInNumber=Number(scoree)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" converted to 33
+// "5fh"=> nan
+// if we take values as true/false it return boolean
+
+let isloggedin=''
+// convert to boolean
+let booleanisloggedin=Boolean(isloggedin)
+console.log(typeof(isloggedin))
+console.log(typeof(booleanisloggedin))
+console.log(booleanisloggedin)
+
+// 1 ->true, 0->false
+// " " =>false
+// "apuu" =>true
+
+let num=22
+let stringnumber=String(num)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+
+
