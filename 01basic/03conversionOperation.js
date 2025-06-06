15b0b2a6ef982cbbc5c822473db53a0220ffb9ea
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-06-06 20:10:08 +0000

    conversion

--- 01basic/03conversionOperation.js
+++ 01basic/03conversionOperation.js
@@ -1,11 +1,11 @@
 let score=false
 let scoree="22a"
-console.log(typeof(score))
-console.log(typeof scoree)
+// console.log(typeof(score))
+// console.log(typeof scoree)
 //  it converts string type into nmber
 let valueInNumber=Number(scoree)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33" converted to 33
 // "5fh"=> nan
@@ -14,9 +14,9 @@ console.log(valueInNumber)
 let isloggedin=''
 // convert to boolean
 let booleanisloggedin=Boolean(isloggedin)
-console.log(typeof(isloggedin))
-console.log(typeof(booleanisloggedin))
-console.log(booleanisloggedin)
+// console.log(typeof(isloggedin))
+// console.log(typeof(booleanisloggedin))
+// console.log(booleanisloggedin)
 
 // 1 ->true, 0->false
 // " " =>false
@@ -24,8 +24,29 @@ console.log(booleanisloggedin)
 
 let num=22
 let stringnumber=String(num)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+// ***********************OPERATIONS***********************
+// console.log("1"+2);  //12
+// console.log(1+"2");  //12
+// console.log("1"+"2");  //12
+// console.log("1"+2+2);  //122 
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
+
+
+
+ 
 
 
 
