b3026a416ac872455615a2c21a96de9f3c4dad6c
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-07-01 10:16:45 +0000

    destructureObject

---
+++ 02basic/05objects.js
@@ -0,0 +1,27 @@
+// de-structuring occurs for both array and object
+
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    coursename:"javascript"
+}
+//course.coursename
+
+//this is destructuring of obj
+const {coursename:cn}=course
+console.log(cn);//javascript
+
+//-----------API how u write values thta come from database in json----** kisis aur s kaam krana
+// json is, keys are also in keys
+            // {
+            //     name:"appu",
+            //     learning : "javascript"
+            // }
+// api can be in the form of array or object they are in json format
+
+            [
+                {},
+                {}
+            ]
+
