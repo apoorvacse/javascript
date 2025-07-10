8bc3b7803f987968b6d7d35ec4b99b4ee2cc185b
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-07-10 12:34:09 +0000

    loops

---
+++ 05iterations/02while.js
@@ -0,0 +1,13 @@
+let i=0
+while(i<12){
+    console.log(`multiple of 3 are ${i}`);
+    i+=3;
+    
+}
+
+let rank=100
+do{
+    console.log(`try until u score rank under${rank}`);
+    rank -= 10;
+    
+}while(rank>=1);
