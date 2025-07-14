0a7428e6eb9872bddb43100b883b4327ac019eb8
Apoorva Singh <129685480+apoorvacse@users.noreply.github.com>
2025-07-14 13:02:34 +0000

    methods of function

---
+++ 05iterations/filtermap.js
@@ -0,0 +1,8 @@
+// callbackFn is invoked only for array indexes which have assigned values. 
+// It is not invoked for empty slots in sparse arrays.
+/*
+The reduce() method is an iterative method. It runs a "reducer" callback function over
+ all elements in the array, in ascending-index order, and accumulates them into a single value.
+*/
+// map,forach, filter, reduce are some array method
+// chaining process
